f0443e6c34a59d5c050be6f057a8e1e62cccfd85
rawiabh <rawiabenhmida@gmail.com>
2018-01-11 16:06:13 +0100

    Add files via upload

--- facebook.py
+++ facebook.py
@@ -1,22 +1,25 @@
 
 # coding: utf-8
 
-# In[23]:
+# In[ ]:
 
 
-token= "EAACRyxtuJXcBAI1FeyL8nHuhhMogYneKEdmZAcCwfC31bhzlhfzvvX0EYxlKlrSLZBQ9t9dfgBWYZCh6KWmrJojtr9SItSZArGCaUnmMj5SRi3K7oMh7eyjKdEAqGeOOEXrsQk4GDAoXPFh4D6d6ALam3v6ZAcbqGaJ99GQ3ZAWPIdV53RZAocjYI7kOY7Ief8rl0bq8ZCweBAZDZD"
+from flask import Flask
+import json
+import urllib.request as ur
+import requests
+app = Flask(__name__)
+
+token= "EAACRyxtuJXcBAFZCuhpkL8FfrgEDWtsa5LVT8DVGGpCZA68AdxqfJv6YI5RotUtqsZBHrEAKGcpcQxZCZBroonZCXeC2MYgBzBlvZCg4W05fF8sb5Tc8vt0KPDsaq7gQXamZBrs45XRPFEJ5yvp36SfnZBZC2lbbx9EjxkJ6Pqeg1rtTK4n6W1SJZAt9QvPbRLSda5IvZBwCf2OwiQZDZD"
 me="https://graph.facebook.com/v2.10/me?access_token="+token
 friends="https://graph.facebook.com/v2.10/me/friends?access_token="+token
 search="https://graph.facebook.com/v2.10/search?q=zara&type=page&access_token="+token
 
+@app.route('/')
+def hello_world():
+    res=requests.get(search)
+    return res.text
 
-# In[24]:
-
-
-import json
-import urllib.request as ur
-import requests
-res=requests.get(search)
-res.text
-
+if __name__ == '__main__':
+  app.run()
 
